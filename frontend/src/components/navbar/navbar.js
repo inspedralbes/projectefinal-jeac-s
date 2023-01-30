cebbf18c134df4a4a701dfe90be77ad487c00750
a20erigasgar <a20erigasgar@inspedralbes.cat>
2023-01-30 12:37:03 +0100

    Rutas funcionales

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,21 +1,30 @@
-import  { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Navbar() {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-          <img src="jeacsLogo.PNG" width="70" height="70" alt=""></img>
-        </a>
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="jeacsLogo.PNG" width="70" height="70" alt=""></img>
+      </a>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-item nav-link"><NavLink to="/">Home</NavLink></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-item nav-link"><NavLink to="/games">Games</NavLink></a>
+        </li>
+      </ul>
 
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav ml-auto">
-            <a class="nav-item nav-link active" href="#">Sign In <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Log In</a>
-            <a class="nav-item nav-link"><NavLink to="/games">Games</NavLink></a>
-          </div>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+
+          <a class="nav-item nav-link active" href="#">Sign In <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Log In</a>
+
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar;

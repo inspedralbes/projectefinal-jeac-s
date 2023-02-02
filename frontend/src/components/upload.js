9ef086e8480107e7da17a31922107a1ccfde211f
Albert-de-la-Orza <A20albmar@inspedralbes.cat>
2023-02-02 11:54:38 +0100

    Formulario para subir juegos

--- frontend/src/components/upload.js
+++ frontend/src/components/upload.js
@@ -1,12 +1,13 @@
+import UploadForm from "./UploadForm";
+
 function Upload() {
     return (
         <div>
             <h1>Upload</h1>
-            <form enctype="multipart/form-data" method="post" action="">
-                <label>Choose a zip file to upload: <input type="file" name="zip_file" /></label>
-                <br />
-                <input type="submit" name="submit" value="Upload" />
-            </form>
+            <h2>How to upload a game</h2>
+            <p>lkkdfldslflsldjkfskajdkfjkjdfkjkdlfjdss</p>
+
+            <UploadForm />
             
         </div>
     )

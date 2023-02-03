d6f1cfaf5e37795a1cc043c12906f3369b6adb47
a20erigasgar <a20erigasgar@inspedralbes.cat>
2023-02-03 09:29:11 +0100

    Mejora front

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -7,19 +7,26 @@ function Navbar() {
         <img src="jeacsLogo.PNG" width="70" height="70" alt=""></img>
       </a>
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-item nav-link"><NavLink to="/">Home</NavLink></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-item nav-link"><NavLink to="/games">Games</NavLink></a>
-        </li>
+        <NavLink to="/">
+          <li class="nav-item active">
+            <a class="nav-item nav-link">Home</a>
+          </li>
+        </NavLink>
+        <NavLink to="/games">
+          <li class="nav-item">
+            <a class="nav-item nav-link">Games</a>
+          </li>
+        </NavLink>
       </ul>
 
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ml-auto">
-
-          <a class="nav-item nav-link active"><NavLink to="/SignIn">Sign In</NavLink></a>
-          <a class="nav-item nav-link"><NavLink to="/login">Log In</NavLink></a>
+          <NavLink to="/SignIn">
+            <a class="nav-item nav-link active">Sign In</a>
+          </NavLink>
+          <NavLink to="/login">
+            <a class="nav-item nav-link">Log In</a>
+          </NavLink>
 
         </div>
       </div>

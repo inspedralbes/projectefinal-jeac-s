db013cd245fcff9c16a666717df91b8ed8fa6cf0
unknown <a21cargomfue@inspedralbes.cat>
2023-02-06 09:38:47 +0100

    Front de registro de usuario hecho

--- frontend/src/components/signin.js
+++ frontend/src/components/signin.js
@@ -1,22 +1,53 @@
 import { Form, Button, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+
 function Signin() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+        const response = await fetch('http://localhost:8000/api/setPlayerData', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',          
+            },
+            body: JSON.stringify({ name, email, password }),
+        });
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        } catch (error) {
+        setError(error);
+        
+        }
+    };
   return (
     <div>
       <Container>
-        <Form>
+        <Form onSubmit={handleSubmit}>
+            {error && <p>{error.message}</p>}
           <Form.Group controlId="formBasicName">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter name" />
+            <Form.Control type="text" placeholder="Enter name" value={name} onChange={(event) => setName(event.target.value)}/>
           </Form.Group>
 
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email" value={email} onChange={(event) => setEmail(event.target.value)}/>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}/>
           </Form.Group>
 
           <Button variant="primary" type="submit">

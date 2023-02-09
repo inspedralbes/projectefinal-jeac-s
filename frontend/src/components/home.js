a737b304b91f62c366463f71e754009cde1bf55f
a20erigasgar <a20erigasgar@inspedralbes.cat>
2023-02-09 09:52:28 +0100

    Carousel arreglado

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom"
 function Home() {
     return (
         <div>
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -14,21 +14,21 @@ function Home() {
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="games.jpg" class="d-block w-100" alt="..."></img>
+                        <img src="games.jpg" class="d-block w-100" alt="Games"></img>
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>First slide label</h5>
                                 <p>Some representative placeholder content for the first slide.</p>
                             </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="mhw2.png" class="d-block w-100" alt="..."></img>
+                        <img src="mhw.png" class="d-block w-100" alt="MHW"></img>
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Second slide label</h5>
                                 <p>Some representative placeholder content for the second slide.</p>
                             </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="valo.jfif" class="d-block w-100" alt="..."></img>
+                        <img src="valo.jfif" class="d-block w-100" alt="Valo"></img>
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Third slide label</h5>
                                 <p>Some representative placeholder content for the third slide.</p>

91c79d31dc49acaf36ffba970e60a228fc72e6bf
JaumePerez <a21jaupersen@inspedralbes.cat>
2023-02-09 12:34:02 +0100

    navbar cuando loged in o no

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,45 +1,13 @@
-import { NavLink } from "react-router-dom"
+import React from 'react';
+import { Provider } from 'react-redux';
+import NavBar from './navbar_provider';
+import { store } from '../store';
 
-function Navbar() {
-  return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-        <img src="jeacsLogo.PNG" width="70" height="70" alt=""></img>
-      </a>
-      <ul class="navbar-nav mr-auto">
-        <NavLink to="/">
-        <li class="nav-item">
-          <a class="nav-item nav-link">Home</a>
-        </li>
-        </NavLink>
-        <NavLink to="/games">
-        <li class="nav-item">
-          <a class="nav-item nav-link">Games</a>
-        </li>
-        </NavLink>
-        <NavLink to="/upload">
-        <li class="nav-item">
-          <a class="nav-item nav-link">Upload</a>
-        </li>
-        </NavLink>
-      </ul>
 
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ml-auto">
-        <NavLink to="/profile">
-            <a class="nav-item nav-link">Profile</a>
-          </NavLink>
-          <NavLink to="/SignIn">
-            <a class="nav-item nav-link">Sign In</a>
-          </NavLink>
-          <NavLink to="/login">
-            <a class="nav-item nav-link">Log In</a>
-          </NavLink>
-            <a class="nav-item nav-link">Log Out</a>
-        </div>
-      </div>
-    </nav>
-  )
-}
+const App = () => (
+  <Provider store={store}>
+    <NavBar />
+  </Provider>
+);
 
-export default Navbar;
+export default App;
---
+++ frontend/src/components/navbar/navbar_provider.js
@@ -0,0 +1,54 @@
+import { NavLink } from "react-router-dom"
+import { useSelector } from 'react-redux';
+
+function Navbar() {
+  const data = useSelector(state => state.data);
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="jeacsLogo.PNG" width="70" height="70" alt=""></img>
+      </a>
+      <ul class="navbar-nav mr-auto">
+        <NavLink to="/">
+          <li class="nav-item">
+            <a class="nav-item nav-link">Home</a>
+          </li>
+        </NavLink>
+        <NavLink to="/games">
+          <li class="nav-item">
+            <a class="nav-item nav-link">Games</a>
+          </li>
+        </NavLink>
+        <NavLink to="/upload">
+          <li class="nav-item">
+            <a class="nav-item nav-link">Upload</a>
+          </li>
+        </NavLink>
+      </ul>
+
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+       
+          {isLoggedIn ?
+             <div class="navbar-nav ml-auto">
+              <NavLink to="/profile">
+                <a class="nav-item nav-link">Profile</a>
+              </NavLink>
+              <a class="nav-item nav-link">Log Out</a>
+            </div> : 
+            <div class="navbar-nav ml-auto">
+              <NavLink to="/SignIn">
+                <a class="nav-item nav-link">Sign In</a>
+              </NavLink>
+
+              <NavLink to="/login">
+                <a class="nav-item nav-link">Log In</a>
+              </NavLink>
+            </div>}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;

1b49c5487637a010b08e9e7d60410f4b81bcf73c
a20erigasgar <a20erigasgar@inspedralbes.cat>
2023-02-10 13:11:15 +0100

    UploadGame

--- frontend/src/components/UploadForm.js
+++ frontend/src/components/UploadForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import { Card, Row, Col, Form, Button, Container, NavLink } from 'react-bootstrap';
 
 const UploadForm = () => {
     const [name, setName] = useState('')
@@ -22,10 +22,10 @@ const UploadForm = () => {
         for (var i = 0; i < byteString.length; i++) {
             ia[i] = byteString.charCodeAt(i);
         }
-        var blob = new Blob([ab], {type: mimeString});
+        var blob = new Blob([ab], { type: mimeString });
         return blob;
-      
-      }
+
+    }
 
 
 
@@ -35,21 +35,21 @@ const UploadForm = () => {
         var reader = new FileReader();
         var imagen = new FileReader();
 
-        reader.onload = function() {
-        let blobZip = dataURItoBlob(reader.result)
-        let blobImg = dataURItoBlob(reader.result)
+        reader.onload = function () {
+            let blobZip = dataURItoBlob(reader.result)
+            let blobImg = dataURItoBlob(reader.result)
 
-        hacerFetch(blobZip, blobImg);
+            hacerFetch(blobZip, blobImg);
         };
-        imagen.onload = function() {
+        imagen.onload = function () {
             let blobImg = dataURItoBlob(imagen.result)
-    
+
             hacerFetch(blobImg);
         };
         reader.readAsDataURL(file);
         reader.readAsDataURL(fileImagen);
 
-        async function hacerFetch(blobZip, blobImg){
+        async function hacerFetch(blobZip, blobImg) {
             try {
                 let fecha = new Date();
                 let diaActual = fecha.getDate();
@@ -71,69 +71,103 @@ const UploadForm = () => {
                 const response = await fetch('http://localhost:8000/api/upload', {
                     method: 'POST',
                     headers: {
-                        'Accept':'*/*'
+                        'Accept': '*/*'
                     },
                     body: formData,
                 });
-        
+
                 if (!response.ok) {
-                throw new Error(response.statusText);
+                    throw new Error(response.statusText);
                 }
-        
+
                 const data = await response.json();
                 console.log(data);
             } catch (error) {
                 setError(error);
-        
+
             }
         }
-        
+
     }
     return (
-        <form>  
-            <div>
-                <label>Name Game</label>
-                <input type='text' placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}></input>
-            </div>
+        <div>
+            <Container>
+                <Row className="d-flex justify-content-center align-items-center">
+                    <Col md={8} lg={6} xs={12}>
+                        <br></br>
+                        <Card className="px-4 rounded bg-secondary">
+                            <Card.Body>
+                                <div className="mb-3 mt-md-4">
+                                    <h2 className="fw-bold mb-2 text-center text-uppercase text-light ">
+                                        Upload a Game
+                                    </h2>
+                                    <div className="mb-3">
+                                        <Form>
+                                            <Form.Group controlId="formBasicNameGame">
+                                                <Form.Label className='text-light'>Name Game</Form.Label>
+                                                <Form.Control type='text' placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                                            </Form.Group>
+                                            <br></br>
 
-            <div>
-                <label>Image Game</label>
-                <input id='uploadImg' type='file' accept="image/png, image/jpeg" value={img} onChange={(e) => setImg(e.target.value)}></input>
-            </div>
+                                            <Form.Group controlId="formBasicImageGame">
+                                                <Form.Label className='text-light'>Image Game</Form.Label>
+                                                <Form.Control id='uploadImg' type='file' accept="image/png, image/jpeg" value={img} onChange={(e) => setImg(e.target.value)} />
+                                            </Form.Group><br></br>
 
-            <div>
-                <label>Zip Game</label>
-                <input id='uploadZip' type='file' accept='.zip' value={zip} onChange={(e) => setZip(e.target.value)}></input>
-            </div>
+                                            <Form.Group controlId="formBasicZipGame">
+                                                <Form.Label className='text-light'>Zip Game</Form.Label>
+                                                <Form.Control id='uploadZip' type='file' accept='.zip' value={zip} onChange={(e) => setZip(e.target.value)} />
+                                            </Form.Group><br></br>
 
-            <div>
-                <label>Description</label>
-                <textarea placeholder="Add a description" rows='5' cols='50' value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
-            </div>
-            <div>
-                <label>Categories</label>
-                    <div>
-                        <input type="checkbox" id="Arcade" name="categories" value="arcade" />
-                        <label htmlFor="Arcade">Arcade</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="Multiplayer" name="categories" value="multiplayer" />
-                        <label htmlFor="Multiplayer">Multiplayer</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="Platformer" name="categories" value="platformer" />
-                        <label htmlFor="Platformer">Platformer</label>
-                    </div>
-            </div>
+                                            <Form.Group controlId="formBasicDescription">
+                                                <Form.Label className='text-light'>Description</Form.Label>
+                                                <Form.Control placeholder="Add a description" rows='5' cols='50' value={description} onChange={(e) => setDescription(e.target.value)} />
+                                            </Form.Group><br></br>
+
+                                            <Form.Group controlId="formBasicCategories">
+                                                <Form.Label className='text-light'>Categories</Form.Label>
+                                                {['checkbox'].map((type) => (
+                                                    <div key={`inline-${type}`} className="mb-3 text-light">
+                                                        <Form.Check
+                                                            inline
+                                                            label="Arcade"
+                                                            name="group1"
+                                                            type={type}
+                                                            id={`inline-${type}-1`}
+                                                        />
+                                                        <Form.Check
+                                                            inline
+                                                            label="Action"
+                                                            name="group1"
+                                                            type={type}
+                                                            id={`inline-${type}-2`}
+                                                        />
+                                                        <Form.Check
+                                                            inline
+                                                            label="Figthing"
+                                                            name="group1"
+                                                            type={type}
+                                                            id={`inline-${type}-3`}
+                                                        />
+                                                    </div>
+                                                ))}
+                                            </Form.Group><br></br>
+
+                                            <Button variant="primary" type="submit" onClick={onClick}>
+                                                Submit
+                                            </Button>
+
+                                        </Form>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
 
 
-
-            <button onClick={onClick}>Upload Game</button>
-
-
-        </form>
-
-        
     )
 }
 

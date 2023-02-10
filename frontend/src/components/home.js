eb4df8582c73861eea7568929a7ce2c5d9737405
JaumePerez <a21jaupersen@inspedralbes.cat>
2023-02-10 09:43:00 +0100

    fixed merge

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -16,24 +16,24 @@ function Home() {
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src="games.jpg" class="d-block w-100" alt="..."></img>
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
                     </div>
                     <div class="carousel-item">
                         <img src="mhw2.png" class="d-block w-100" alt="..."></img>
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
-                            </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
                     </div>
                     <div class="carousel-item">
                         <img src="valo.jfif" class="d-block w-100" alt="..."></img>
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
-                            </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -45,5 +45,8 @@ function Home() {
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+        </div>
+    )
+}
 
 export default App;

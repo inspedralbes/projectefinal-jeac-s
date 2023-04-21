ea6adc18bf812b322551a1fc27edc881c44936df
a20erigasgar <a20erigasgar@inspedralbes.cat>
2023-04-21 09:33:40 +0200

    Cambios

--- frontend/src/components/provider_asideNav.js
+++ frontend/src/components/provider_asideNav.js
@@ -47,9 +47,14 @@ function AsideNav() {
 
             <aside class="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
 
-                 <NavLink to="/profile">
+
+                <NavLink to="/profile">
                     <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+                        {isLoggedIn ? 
                         <img class="h-10 w-12 rounded-full" src="pf3.PNG" alt=""></img>
+                            : <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" /></svg>
+                        }
+
                     </div>
                 </NavLink>
 

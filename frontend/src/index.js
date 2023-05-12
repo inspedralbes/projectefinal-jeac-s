b20d59f4eee342789687ca0125e31c16e611cab5
CarlosGomezFuentes <a21cargomfue@inspedralbes.cat>
2023-05-12 11:48:13 +0200

    URL fetch cambiada

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,8 +9,8 @@ import {HashRouter} from "react-router-dom";
 import './App.css';
 
 const routes = {
-  fetchLaravel: "http://jeacsg.alumnes.inspedralbes.cat/../laravel/public",
-  wsNode: "http://jeacsg.alumnes.inspedralbes.cat:7878",
+  fetchLaravel: "https://jeacsg.alumnes.inspedralbes.cat:8000",
+  wsNode: "https://jeacsg.alumnes.inspedralbes.cat:7878",
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

a6ee6bcf24366678a0f063715046b49eab9b127a
JaumePerez <a21jaupersen@inspedralbes.cat>
2023-05-24 08:43:21 +0200

    games

--- frontend/src/pages/games.js
+++ frontend/src/pages/games.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { actions } from '../components/store.js';
 
 function Games({ onSharedValueChange, onSharedIdChange }) {
     const [fetchData, setFetchData] = useState([]);

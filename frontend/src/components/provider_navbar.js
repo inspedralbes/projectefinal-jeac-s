bf48cc63cf970a81b958a246c110084449ea16b3
JaumePerez <a21jaupersen@inspedralbes.cat>
2023-05-26 11:09:19 +0200

    traductor

--- frontend/src/components/provider_navbar.js
+++ frontend/src/components/provider_navbar.js
@@ -35,7 +35,7 @@ function Navbar() {
 
                                 <NavLink to="/games">
                                     <button href="#" className=" text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                                        Juegos
+                                    {t('navGames')}
                                     </button>
                                 </NavLink>
 

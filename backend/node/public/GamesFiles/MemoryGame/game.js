8d0548baf524f5d88c7a2d4afa5e34e87ba5df18
carlos <a21cargomfue@inspedralbes.cat>
2023-06-09 18:07:49 +0200

    Juego carlos a medias

--- backend/node/public/GamesFiles/MemoryGame/game.js
+++ backend/node/public/GamesFiles/MemoryGame/game.js
@@ -1,24 +1,13 @@
+import Preloader from "./scenes/Preloader";
+import Game from "./scenes/Game";
+
 const configGame = {
     multiplayer: true,
     singleplayer: true,
     max_players: 4,
 }
 
-// Variables globales
-var cards;
-var flippedCards = [];
-var score = 0;
-var scoreText;
-
-let canvasWidth = 800;
-let canvasHeight = 600;
-
-let textWin;
-let button;
-
-let gameEnded = false;
-
-var game;
+let game;
 
 function init(_sendInfoGame, _finalJuego) {
     //traspasamos la funciones recibidas a variables globales
@@ -38,30 +27,27 @@ function init(_sendInfoGame, _finalJuego) {
                 gravity: { y: 0 }
             }
         },
-        scene: {
-            preload: preload,
-            create: create,
-            // update: update
-        }
+        scene: [Preloader, Game]
     };
 
     game = new Phaser.Game(config);
+
     return game;
 }
 
-// Precarga de los recursos del juego
-function preload() {
-    this.load.image('bear', '../GamesFiles/MemoryGame/images/bear.png');
-    this.load.image('chicken', '../GamesFiles/MemoryGame/images/chicken.png');
-    this.load.image('duck', '../GamesFiles/MemoryGame/images/duck.png');
-    this.load.image('parrot', '../GamesFiles/MemoryGame/images/parrot.png');
-    this.load.spritesheet('sokoban', '../GamesFiles/MemoryGame/images/sockoban_tilesheet.png', {
-        frameWidth: 64
-    });
-    // this.load.spritesheet('');
+// // Precarga de los recursos del juego
+// function preload() {
+//     this.load.image('bear', '../GamesFiles/MemoryGame/images/bear.png');
+//     this.load.image('chicken', '../GamesFiles/MemoryGame/images/chicken.png');
+//     this.load.image('duck', '../GamesFiles/MemoryGame/images/duck.png');
+//     this.load.image('parrot', '../GamesFiles/MemoryGame/images/parrot.png');
+//     this.load.spritesheet('sokoban', '../GamesFiles/MemoryGame/images/sockoban_tilesheet.png', {
+//         frameWidth: 64
+//     });
+//     // this.load.spritesheet('');
 
-}
+// }
 
-function create(){
-    self = this;
-}
+// function create(){
+//     self = this;
+// }
